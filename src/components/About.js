ffa597341b06cfdd8d99bb7c822cc89f9ce0b12b
Sankalpa Poudel <sankalpaeditz@gmail.com>
2026-04-21 20:08:41 +0545

    Increase hero title font size

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,11 @@ export function About() {
       <div class="about-content">
         <h2 class="section-title">About <span class="highlight-purple">Me</span></h2>
         <p class="about-text">
-          Hi! I'm Sankalpa Poudel, a passionate Website Developer and Cyber Security Enthusiast. Since starting my journey on March 10, 2025, I've been diving deep into web development and exploring the roles of an attacker and defender in the cyber world. I love building cool things for the web and understanding how to protect systems. Let's build something secure and amazing together!
+          Hi,! I am Sankalpa poudel . I am currently learning by Bachelor,s degree in Bachelor Of Science (HONOROURS) IN INFORMATION TECHNOLOGY 
+          At techspire college and Affliated to Aisa pasafic University of Technology (APU) . I am passionate about cyber security and web development.
         </p>
+           
+
         <div class="skills">
           <span class="skill-tag">Cyber Security</span>
           <span class="skill-tag">Web Development</span>
